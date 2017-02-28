d45cdba4d49e25fa72c8997016c0aa69fbb98978
Bongatoughy <lewiskyler@gmail.com>
2017-02-27 22:38:55 -0500

    Initialize repo

---
+++ main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let play = document.querySelector(".play");
+  let pause = document.querySelector(".pause");
+  let aud = new Audio('ravel.m4a');
+
+  play.addEventListener("click", () => {
+    aud.play();
+  });
+
+  pause.addEventListener("click", () => {
+    aud.pause();
+  });
+});
